d110f4d8958d2d9c78d1e9767d82cdc63fb653ba
Evgeniy <evgeniykovalsky04386@gmail.com>
2022-08-12 21:54:50 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+function App(props) {
+
+ const[description,setDescription]= useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+       <div className="conteiner">
+    <div className="head">{props.data.book}</div>
+
+    <div className="mainBody">
+        <div className="picture"> <img src={props.data.image} alt={props.data.id}/></div>
+        <div className="description">
+            <p><span>АВТОР</span> </p>
+            <p><span>Имя:</span> {props.data.Name}</p>
+            <p><span>Фамилия:</span> {props.data.SurName}</p>
+            <p><span>Название книги:</span> {props.data.book}</p>
+          
+            <p className="lsDesc">{props.data.description}</p>
+           
+        </div>
     </div>
+    <div className="footer">
+        <button 
+            onClick={()=>setDescription(prev =>!prev)}
+            >{description? 'Cкрыть':'Подробнее..'}</button>
+            {description && <p>{props.data.allDescription}</p>}
+    </div>
+    
+ </div>
+   
   );
 }
 
